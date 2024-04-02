2e28de093fbb2fc5ab49d55f6bd12582add49abe
Harshit_Dahiya <dahiya.harshit12@gmail.com>
2024-04-02 12:13:26 +0530

    attaching payload to actions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,30 @@
 import classes from './Counter.module.css';
-import { useSelector } from 'react-redux';
+import { useSelector ,useDispatch} from 'react-redux';
 
 const Counter = () => {
-  const counter=useSelector(state=>state.counter)
+  const counter=useSelector(state=>state.counter);
+  const dispatch=useDispatch();
   const toggleCounterHandler = () => {};
 
+  const handleIncrement=()=>{
+      dispatch({type:"increment"})
+  }
+  const handleIncrease=()=>{
+    dispatch({type:"increase",amount:10})
+}
+  const handleDecrement =()=>{
+    dispatch({type:"decrement"})
 
+  }
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleIncrease}>Increase by 10</button>
+      <button onClick={handleDecrement}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
